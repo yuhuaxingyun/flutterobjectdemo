283dd56420975ce86c9ff0408ee78b71d0552065
宋江 <2496157421@qq.com>
2020-07-27 15:21:24 +0800

    添加支付宝支付

---
+++ build/sy_flutter_alipay/generated/source/buildConfig/debug/alipay/flutter/isanye/cn/syflutteralipay/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package alipay.flutter.isanye.cn.syflutteralipay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "alipay.flutter.isanye.cn.syflutteralipay";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "alipay.flutter.isanye.cn.syflutteralipay";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
--- lib/PersonPage.dart
+++ lib/PersonPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterobjectdemo/pay/pay_page.dart';
+import 'package:dio/dio.dart';
 
 class PersonalPage extends StatefulWidget{
   @override
@@ -7,16 +9,32 @@ class PersonalPage extends StatefulWidget{
   }
 }
 
-class PersonalState extends State<PersonalPage>{
+class PersonalState extends State<PersonalPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar (
-          title: new Text("我的"),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.list), onPressed: null),
+      appBar: new AppBar (
+        title: new Text("我的"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: null),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            RaisedButton(
+              child: Text('支付',style: TextStyle(fontSize: 24),),
+              onPressed: (){
+                Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context){
+                  return PayPage();
+                }));
+              },
+            ),
           ],
         ),
+      ),
     );
   }
 }
